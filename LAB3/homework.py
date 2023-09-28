3a8c01f48e608c2660211a4c164b1e1c1b9b0ed7
Buzu Alexandru <mr.buzu00@gmail.com>
2023-09-29 00:20:48 +0300

    lab 3

--- LAB3/homework.py
+++ LAB3/homework.py
@@ -1,49 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 
 def scrapper(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    info = dict()
+        info = dict()
 
-    info["Product"] = soup.find('h1').get_text()
+        try:
+            info["Product"] = soup.find('h1').get_text()
+        except:
+            pass
 
-    info["Seller"] = soup.find('a', {'class': 'adPage__aside__stats__owner__login buyer_experiment'}).get_text()
+        try:
+            info["Seller"] = soup.find('a', {'class': 'adPage__aside__stats__owner__login buyer_experiment'}).get_text()
+        except:
+            pass
 
-    info["Last update"] = soup.find('div', {'class': 'adPage__aside__stats__date'}).get_text()[19:]
+        info["Last update"] = (soup.find('div', {'class': 'adPage__aside__stats__date'})
+                               .get_text().replace('Data actualizării: ', ''))
 
-    info["Transaction"] = soup.find('div', {'class': 'adPage__aside__stats__type'}).get_text()[7:]
+        info["Transaction"] = (soup.find('div', {'class': 'adPage__aside__stats__type'})
+                               .get_text().replace('Tipul: ', ''))
 
-    info["Price"] = soup.find('span', {'class': 'adPage__content__price-feature__prices__price__value'}).get_text()
-    currency = soup.find('span', {'class': 'adPage__content__price-feature__prices__price__currency'})
-    if currency:
-        info["Price"] += currency.get_text()
+        info["Price"] = soup.find('span', {'class': 'adPage__content__price-feature__prices__price__value'}).get_text()
+        currency = soup.find('span', {'class': 'adPage__content__price-feature__prices__price__currency'})
+        if currency:
+            info["Price"] += currency.get_text()
 
-    characteristics = []
-    info['Extra'] = []
-    for div in soup.findAll('div', {'class': 'adPage__content__features'}):
+        characteristics = []
+        info['Extra'] = []
+        for div in soup.findAll('div', {'class': 'adPage__content__features'}):
 
-        for characteristics_div in div.findAll('div', {'class': 'adPage__content__features__col grid_9 suffix_1'}):
-            for ul in characteristics_div.findAll('ul'):
-                for link in ul.findAll('span'):
-                    characteristics.append(link.text.lstrip().rstrip())
+            for characteristics_div in div.findAll('div', {'class': 'adPage__content__features__col grid_9 suffix_1'}):
+                for ul in characteristics_div.findAll('ul'):
+                    for attribute in ul.findAll('span'):
+                        characteristics.append(attribute.text.lstrip().rstrip())
 
-            for i in range(0, len(characteristics) - 1, 2):
-                info.update({characteristics[i]: characteristics[i + 1]})
+                for i in range(0, len(characteristics) - 1, 2):
+                    info.update({characteristics[i]: characteristics[i + 1]})
 
-        # get the options that are listed
-        for options_div in div.findAll('div', {'class': 'adPage__content__features__col grid_7 suffix_1'}):
-            for ul in options_div.findAll('ul'):
-                for link in ul.findAll('span'):
-                    info["Extra"].append(link.text.lstrip().rstrip())
+            for options_div in div.findAll('div', {'class': 'adPage__content__features__col grid_7 suffix_1'}):
+                for ul in options_div.findAll('ul'):
+                    for attribute in ul.findAll('span'):
+                        info["Extra"].append(attribute.text.lstrip().rstrip())
 
-    info.update({"Description": soup.find('div', {'class': 'adPage__content__description grid_18'}).get_text()})
+        info.update({"Description": soup.find('div', {'class': 'adPage__content__description grid_18'}).get_text()})
 
-    return info
+    # except:
+    #     print('Invalid link')
+    #     sys.exit()
+
+        return info
 
 
 product = 'https://999.md/ro/82333904'
+# product = 'https://999.md/ro/84339368'
 for k, v in scrapper(product).items():
     print(f'{k} : {v}')
